bd9c05dbeb51da000c4e16fa980c652bbf1437aa
ChristopherBrislin <65586758+ChristopherBrislin@users.noreply.github.com>
2021-01-12 15:45:26 +1100

    Intent extra with email address string.

--- app/src/main/java/com/example/radcolour/MainActivity.java
+++ app/src/main/java/com/example/radcolour/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.radcolour;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
+
 
 import android.app.Activity;
-import android.content.DialogInterface;
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -14,33 +13,35 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
-
+    private String address;
+    public static String EMAIL_ADDRESS = "com.example.radcolour.EMAIL_ADDRESS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText email = (EditText)findViewById(R.id.email_entry);
-        TextView validEmail = (TextView)findViewById(R.id.valid_email);
-        Button submit = (Button) findViewById(R.id.pick_colour);
+        EditText email = findViewById(R.id.email_entry);
+        TextView validEmail = findViewById(R.id.valid_email);
+        Button submit = findViewById(R.id.pick_colour);
 
-        email.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+        email.setOnFocusChangeListener(new View.OnFocusChangeListener() {
            @Override
            public void onFocusChange(View view, boolean hasFocus){
                if(!hasFocus){
+                   /*
+                   On focus change hide the keyboard and check the
+                   text field for a valid address.
+                    */
                    hideKeyboard(view);
-                   if(isValidEmail(email.getText().toString())){
+                   if(isValidEmail(address=email.getText().toString())){
                        validEmail.setText(R.string.valid_email);
                        submit.setVisibility(View.VISIBLE);
                    }else{
+                       //If not valid, hide the button.
                        validEmail.setText(R.string.not_valid_email);
                        submit.setVisibility(View.INVISIBLE);
                    }
@@ -51,8 +52,7 @@ public class MainActivity extends AppCompatActivity {
         email.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-
+                //
             }
 
             @Override
@@ -62,10 +62,15 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-               if(isValidEmail(email.getText().toString())){
+                /*
+                Edit text change listener checks the text field for a valid
+                email address anytime the content is changed.
+                 */
+               if(isValidEmail(address=email.getText().toString())){
                    validEmail.setText(R.string.valid_email);
                    submit.setVisibility(View.VISIBLE);
                }else{
+                   //If not valid, hide the button.
                    validEmail.setText(R.string.not_valid_email);
                    submit.setVisibility(View.INVISIBLE);
                }
@@ -78,18 +83,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean isValidEmail(String email){
-
+        //Returns true if a valid email address string.
         return Patterns.EMAIL_ADDRESS.matcher(email).matches();
 
     }
 
     public void hideKeyboard(View view){
+        //Hide the keyboard.
         InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
     }
 
 
     public void onSubmit(View view){
+        Intent intent = new Intent(this, ColourPicker.class);
+        intent.putExtra(EMAIL_ADDRESS, address);
+
+        startActivity(intent);
 
     }
 
